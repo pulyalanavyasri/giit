4bc867499e66b519771e306c9991048ccfc81a56
navya reddy <pulyala.navyasri@gmail.com>
2026-04-03 11:42:15 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,20 @@
+package com.pulyalanavyasri.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Java Spring Boot on Kubernetes!";
     }
 }
